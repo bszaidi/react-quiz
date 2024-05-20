1d31ca00375f13ce25d53cc9e9439c0b00fad625
bszaidi <szaidi@squareup.com>
2024-05-20 14:07:15 +1000

    Installed NPM Packaged json-server and editied package-lock to be able to run the data server. useEffect to ftch data from the server we made. usereducer for state management,

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
 function App() {
-  return <div className="App"></div>;
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return { ...state, questions: action.payload, status: "ready" };
+      case "dataFailed":
+        return { ...state, status: "error" };
+      default:
+        throw new Error("Action unknown");
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //Using useEffect to set the title of the document
+  useEffect(() => {
+    document.title = "The React Quiz";
+  }, []);
+  //Using useEffect to fetch data from the server
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="App">
+      <Header />
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;

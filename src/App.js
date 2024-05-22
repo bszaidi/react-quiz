1121c2c1dd0645152212483ee4c7d75bb93f565f
bszaidi <szaidi@squareup.com>
2024-05-22 15:57:52 +1000

    Handling new answers and points

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     questions: [],
     status: "loading",
     index: 0,
+    answer: null,
+    points: 0,
   };
   function reducer(state, action) {
     switch (action.type) {
@@ -20,11 +22,15 @@ function App() {
         return { ...state, status: "error" };
       case "start":
         return { ...state, status: "active" };
+      case "newAnswer":
+        const question = state.questions.at(state.index);
+        return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points };
+
       default:
         throw new Error("Action unknown");
     }
   }
-  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
   //Using useEffect to set the title of the document
   useEffect(() => {
@@ -44,7 +50,7 @@ function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
       </Main>
     </div>
   );
